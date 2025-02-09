f3cec344ea3996198ccdc87fdfafca36c1ce4f2b
S M A Tousi <mohamadali.tousi@gmail.com>
2025-02-09 16:34:46 -0600

    added

---
+++ generate-gpu-jobs-for-unimatch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# vary the joints
+seed_start=1
+seed_end=5
+seed_variation=1
+
+model_size=('dino_small' 'dino_base' 'dino_large')
+
+method=('unimatch_v2_wandb_wo_FA_normloss_gradient'
+        'unimatch_v2_wandb_gradient' 
+        'unimatch_v2_wandb_normloss_gradient' 
+        'unimatch_v2_wandb_normloss' 
+        # 'unimatch_v2_wandb_wo_FA_gradient' 
+        # 'unimatch_v2_wandb_wo_FA_normloss' 
+        # 'unimatch_v2_wandb_wo_FA' 
+        # 'unimatch_v2_wandb'
+        )
+
+# Create jobs for each value in the range of seeds, model sizes, and methods
+for size in "${model_size[@]}"; do
+  for meth in "${method[@]}"; do
+    for (( seed=seed_start; seed<=seed_end; seed+=seed_variation )); do
+      export seed="$seed"
+      export size="$size"
+      export meth="$meth"
+      export job_name="${meth//_/-}-${size//_/-}-${seed}"  # Only replace underscores in the method part of the job_name
+      export job_name="${job_name,,}"
+
+      # Use environment variables in your job template and apply it
+      envsubst < unimatch_job.yaml | kubectl apply -f -
+    done
+  done
+done
